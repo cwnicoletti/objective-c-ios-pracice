3e9fe9408af51cf677a07d0bc56ec4dbbe27163f
cwnicoletti <cwnicoletti@gmail.com>
2021-08-11 12:02:33 -0700

    Adding first objective-c project

---
+++ practiceApp/practiceApp/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  practiceApp
+//
+//  Created by Christian Nicoletti on 8/11/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ practiceApp/practiceApp/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  practiceApp
+//
+//  Created by Christian Nicoletti on 8/11/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
